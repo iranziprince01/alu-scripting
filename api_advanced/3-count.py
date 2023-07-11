79585b0d5d068f1c88985d892ad13d9c54aba18e
Prince Iranzi <116654088+iranziprince01@users.noreply.github.com>
2023-07-11 11:38:41 +0200

    Count it!

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,50 +1,95 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+""" querry reddit api for subreddit info
+"""
 import requests
+import requests.auth
+import string
+from time import sleep
 
 
-def count_words(subreddit, word_list, count_dict={}, after=None):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def authenticate():
+    """ authenticate function
+    doesnt take parameters returns token_type and access_token
     """
-    params = {"limit": 50}
-    if after:
-        params["after"] = after
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={
-            "User-Agent": "underscoDe@alx-holbertonschool"},
-        params=params,
-        allow_redirects=False)
-    if req.status_code == 200:
-        posts = req.json().get("data").get("children")
+    usr_name = "jgadelugo"
+    temp = "HolbertonPass845"
+
+    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
+    client_id = "hy4KvoK0W2iDvw"
+
+    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password",
+                 "username": usr_name,
+                 "password": temp}
+
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+    response = requests.post("https://www.reddit.com/api/v1/access_token",
+                             auth=client_auth, data=post_data, headers=headers)
+    auth_json = response.json()
+
+    token_type = auth_json['token_type']
+    access_token = auth_json['access_token']
+
+    return (token_type, access_token)
+
+
+def recurse(subreddit, hot_list=[], after=[], t_type=None, a_token=None):
+    """ querry reddit api for hot post
+    recursively get all hot post from subreddit
+    """
+    sub = subreddit
+    subreddit = "/r/{}/hot".format(sub)
+    usr_name = "jgadelugo"
+
+    if len(after) == 0:
+        t_type, a_token = authenticate()
+
+    headers = {"Authorization": "{} {}".format(t_type, a_token),
+               "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+    if len(after) != 0:
+        param = {"limit": 100, "after": after[-1]}
+    else:
+        param = {"limit": 100}
+
+    sleep(1)
+    query = "https://oauth.reddit.com{}".format(subreddit)
+    res = requests.get(query, headers=headers, params=param)
+
+    status = res.status_code
+
+    if (status != 200):
+        return None
+    else:
+        data = res.json()
+        if data['data']['after'] in after:
+            return hot_list
+        after.append(data['data']['after'])
+        posts = data["data"]['children']
         for post in posts:
-            title = post.get("data").get("title")
-            word_list = [word.lower() for word in word_list]
+            hot_list.append(post['data']['title'])
+
+        return recurse(sub, hot_list, after, t_type, a_token)
+
+
+def count_words(subreddit, word_list):
+    """ count words """
+    flag = 0
+    words = {}
+    for word in word_list:
+        words[word] = 0
+    hot_list = recurse(subreddit)
+    if hot_list is None:
+        return
+    for hot in hot_list:
+        hot.translate(str.maketrans('', '', string.punctuation))
+        for h in hot.lower().split():
             for word in word_list:
-                w_count = title.split().count(word)
-                if count_dict.get(word):
-                    count_dict[word] += w_count
-                else:
-                    count_dict[word] = w_count
-        if req.json().get("data").get("after"):
-            count_words(
-                subreddit,
-                word_list=word_list,
-                count_dict=count_dict,
-                after=req.json().get("data").get("after"))
-        else:
-            for pair in sorted(
-                    count_dict.items(),
-                    key=lambda kv: (
-                        kv[1],
-                        kv[0]),
-                    reverse=True):
-                if pair[1]:
-                    print("{}: {}".format(pair[0].strip(), pair[1]))
+                if h.lower() == word.lower():
+                    words[word] += 1
+    sorted_words = sorted(words.items(), key=lambda x: (-x[1], x[0]))
+    for key, value in sorted_words:
+        if value != 0:
+            print("{}: {}".format(key, value))
+            flag = 1
+    if flag == 0:
+        print()
